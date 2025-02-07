a6a93aae0f7e9eeae0a28794e8a1c91f096b09e3
receiving1943 <receiving_1943@homedepot.com>
2025-02-07 15:35:22 -0600

    Add files via upload

---
+++ __version__.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 6, 0),
+    prodvers=(1, 0, , 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x4,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'The Home Depot'),
+        StringStruct(u'FileDescription', u'Truck Tracker for JBHunt\xae Trucks'),
+        StringStruct(u'FileVersion', u'1.0.6'),
+        StringStruct(u'InternalName', u'Truck Tracker'),
+        StringStruct(u'LegalCopyright', u'\xa9 The Home Depot. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'TruckTrackerGUI.exe'),
+        StringStruct(u'ProductName', u'Truck Tracker for JBHunt\xae Trucks'),
+        StringStruct(u'ProductVersion', u'1.0.6')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)#Created by Pressley W. @ Store 1943
